68ef2e679fe9d1fd558325ddbcdf796cc41be9bd
tushar <tushar@theprocedure.in>
2020-07-07 02:46:32 +0530

    added toggle feature for color scheme change

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,16 +1,15 @@
-$(window).scroll(function() {
+// Navbar Scroll Function
+$(window).scroll(function () {
     if ($(document).scrollTop() > 50) {
         $('.nav').addClass('affix');
-        console.log("OK");
     } else {
         $('.nav').removeClass('affix');
     }
 });
 
+// Toggles the Responseive Navbar
 $('.navTrigger').click(function () {
     $(this).toggleClass('active');
-    console.log("Clicked menu");
     $("#navList").toggleClass("show_list");
     $("#navList").fadeIn();
-
 });
